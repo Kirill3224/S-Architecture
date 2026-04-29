9aaf812e17c8dc88fd3546ef3807317bc0bd8cfb
Kirill3224 <top1bleach@gmail.com>
2026-04-29 13:59:58 +0300

    feat: setup EFCore connection in Program and add AppDbContext
    - Fixed package bugs

--- ThirdLab/TL.PL/Program.cs
+++ ThirdLab/TL.PL/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine();
+﻿using DotNetEnv;
+using TL.DAL.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+Env.TraversePath().Load();
+
+var dbHost = Env.GetString("DB_HOST", "localhost");
+var dbPort = Env.GetString("DB_PORT", "5432");
+var dbUser = Env.GetString("POSTGRES_USER");
+var dbPass = Env.GetString("POSTGRES_PASSWORD");
+var dbName = Env.GetString("POSTGRES_DB", "ThirdLabDb");
+
+var connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPass};";
+
+var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+optionsBuilder.UseNpgsql(connectionString);
+
+using var context = new AppDbContext(optionsBuilder.Options);
+
+try
+{
+    context.Database.Migrate();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
